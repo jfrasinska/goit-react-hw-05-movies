1b805b8bfa23b591046db4d00ae783de6422de0b
jfrasinska <118769421+jfrasinska@users.noreply.github.com>
2023-12-07 15:08:05 +0000

    Deploying to gh-pages from @ jfrasinska/goit-react-hw-05-movies@06de223ad733d1726042dc75a9e36d1b6490c29f 🚀

---
+++ static/js/287.962cea35.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[287],{287:function(e,t,a){a.r(t),a.d(t,{default:function(){return c}});a(791);var s=a(184),c=function(e){var t=e.cast;return(0,s.jsxs)("div",{children:[(0,s.jsx)("h2",{className:"cast-title",children:"Cast"}),(0,s.jsx)("ul",{className:"cast-list",children:t.map((function(e){return(0,s.jsx)("li",{className:"cast-item",children:e.name},e.id)}))})]})}}}]);
+//# sourceMappingURL=287.962cea35.chunk.js.map
